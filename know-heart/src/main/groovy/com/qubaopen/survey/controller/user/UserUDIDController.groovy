cb47114d8d95a524a0a03482ba9e52818346585d
mars <cosmo_mars@outlook.com>
2014-08-07 13:41:57 +0800

    modify udidController

--- know-heart/src/main/groovy/com/qubaopen/survey/controller/user/UserUDIDController.groovy
+++ know-heart/src/main/groovy/com/qubaopen/survey/controller/user/UserUDIDController.groovy
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 import com.qubaopen.core.controller.AbstractBaseController
@@ -12,6 +13,7 @@ import com.qubaopen.core.repository.MyRepository
 import com.qubaopen.survey.entity.user.UserUDID
 import com.qubaopen.survey.repository.user.UserUDIDRepository
 import com.qubaopen.survey.service.user.UserUDIDService
+import com.qubaopen.survey.utils.DateCommons
 
 @RestController
 @RequestMapping('userUDIDs')
@@ -55,4 +57,33 @@ public class UserUDIDController extends AbstractBaseController<UserUDID, Long> {
 
 		'{"success": 1}'
 	}
+
+	/**
+	 * 修改推送信息
+	 * @param udidId
+	 * @param isPush
+	 * @param startTime
+	 * @param endTime
+	 * @return
+	 */
+	@RequestMapping(value = 'modifyUDID', method = RequestMethod.PUT)
+	modifyUDID(@RequestParam(required = false) long udidId,
+		@RequestParam(required = false) boolean isPush,
+		@RequestParam(required = false) String startTime,
+		@RequestParam(required = false) String endTime) {
+
+		def userUDID = userUDIDRepository.findOne(udidId)
+		if (isPush) {
+			userUDID.push = isPush
+		}
+		if (startTime) {
+			userUDID.startTime = DateCommons.String2Date(startTime, 'HH:mm')
+		}
+		if (endTime) {
+			userUDID.endTime = DateCommons.String2Date(endTime, 'HH:mm')
+		}
+		userUDIDRepository.save(userUDID)
+		'{"success": 1}'
+	}
+
 }

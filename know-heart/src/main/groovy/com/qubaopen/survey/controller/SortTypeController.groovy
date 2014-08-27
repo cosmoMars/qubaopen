fb70184c8b1f24486ec64bd3a686618ce87245ba
mars <cosmo_mars@outlook.com>
2014-08-27 17:42:22 +0800

    add sortType

---
+++ know-heart/src/main/groovy/com/qubaopen/survey/controller/SortTypeController.groovy
@@ -0,0 +1,48 @@
+package com.qubaopen.survey.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qubaopen.core.controller.AbstractBaseController;
+import com.qubaopen.core.repository.MyRepository;
+import com.qubaopen.survey.entity.SortType;
+import com.qubaopen.survey.repository.SortTypeRepository;
+
+@RestController
+@RequestMapping("sortTypes")
+public class SortTypeController extends AbstractBaseController<SortType, Long> {
+
+	@Autowired
+	SortTypeRepository sortTypeRepository;
+
+	@Override
+	protected MyRepository<SortType, Long> getRepository() {
+		sortTypeRepository;
+	}
+	
+	/**
+	 * 获取排序方法名
+	 * @return
+	 */
+	@RequestMapping(value = 'retrieveSortTypes', method = RequestMethod.GET)
+	retrieveSortTypes(){
+		
+		def sortTypes = sortTypeRepository.findAll(),
+			data = []
+		
+		sortTypes.each {
+			data << [
+				'id' : it.id,
+				'name' : it.name
+			]
+		}
+		[
+			'success' : '1',
+			'message' : '成功',
+			'data' : data
+		]
+	}
+}

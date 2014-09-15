5ad4e7760917e2189f09686d80820024f36a08cc
mars <cosmo_mars@outlook.com>
2014-09-15 22:42:22 +0800

    d

---
+++ backend/src/main/java/com/qubaopen/backend/controller/InterestOrderController.java
@@ -0,0 +1,10 @@
+package com.qubaopen.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("backendSelf")
+public class InterestOrderController {
+
+}

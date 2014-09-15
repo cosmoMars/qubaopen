3cef28e0248deaaba659f4c1ec7a203cd0070592
mars <cosmo_mars@outlook.com>
2014-09-15 23:34:12 +0800

    d

--- backend/src/main/java/com/qubaopen/backend/controller/InterestOrderController.java
+++ backend/src/main/java/com/qubaopen/backend/controller/InterestOrderController.java
@@ -1,10 +1,26 @@
 package com.qubaopen.backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RestController
-@RequestMapping("backendSelf")
+@RequestMapping("backendInterestOrder")
 public class InterestOrderController {
 
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	
+	@Transactional
+	@RequestMapping(value = "saveInterestOrder", method = RequestMethod.POST)
+	private String saveInterestOrder(String json) {
+		
+		
+		return null;
+	}
 }

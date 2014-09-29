3e9f19645df368dfaf1793f2718eea89a3d01a64
mars <cosmo_mars@outlook.com>
2014-09-29 15:20:38 +0800

    d

--- know-heart/src/main/groovy/com/qubaopen/survey/controller/point/PointController.groovy
+++ know-heart/src/main/groovy/com/qubaopen/survey/controller/point/PointController.groovy
@@ -19,46 +19,46 @@ public class PointController {
 	getPoint () {
 
 		MatFactory matFactory = new MatFactory();
-		Double[] d1, d2;
-		Object[] o1;
-		d1 = new Double[16];
-		d2 = new Double[16];
-		o1 = new Object[16];
-
-		for (int i = 0; i < 16; i++) {
-			d1[i] = new Double(i);
-			d2[i] = new Double(i);
-			o1[i] = i;
-		}
-
-		MWNumericArray n1 = new MWNumericArray(d1, MWClassID.DOUBLE);
-
-		MWNumericArray n2 = new MWNumericArray(d2, MWClassID.DOUBLE);
-
-		Object[] oList = new Object[2];
-		oList[0] = d1;
-		oList[1] = d2;
-
-		Object[] result = matFactory.createFit(1, n1, n2);
-
-		MWNumericArray a = new MWNumericArray(result[0], MWClassID.DOUBLE);
-
-		//		System.out.println(a);
-		//		System.out.println(a.getDouble());
-
-		MWNumericArray temp = (MWNumericArray) result[0];
-
-		double[][] weights = (double[][]) temp.toDoubleArray();
-		def console = []
-		for (int i = 0; i < weights.length; i++) {
-
-			for (int j = 0; j < weights[i].length; j++) {
-
-				//				System.out.print(i + "," + j + "   :");
-				//				System.out.println(weights[i][j]);
-				console << weights[i][j]
-			}
-		}
-		console
+//		Double[] d1, d2;
+//		Object[] o1;
+//		d1 = new Double[16];
+//		d2 = new Double[16];
+//		o1 = new Object[16];
+//
+//		for (int i = 0; i < 16; i++) {
+//			d1[i] = new Double(i);
+//			d2[i] = new Double(i);
+//			o1[i] = i;
+//		}
+//
+//		MWNumericArray n1 = new MWNumericArray(d1, MWClassID.DOUBLE);
+//
+//		MWNumericArray n2 = new MWNumericArray(d2, MWClassID.DOUBLE);
+//
+//		Object[] oList = new Object[2];
+//		oList[0] = d1;
+//		oList[1] = d2;
+//
+//		Object[] result = matFactory.createFit(1, n1, n2);
+//
+//		MWNumericArray a = new MWNumericArray(result[0], MWClassID.DOUBLE);
+//
+//		//		System.out.println(a);
+//		//		System.out.println(a.getDouble());
+//
+//		MWNumericArray temp = (MWNumericArray) result[0];
+//
+//		double[][] weights = (double[][]) temp.toDoubleArray();
+//		def console = []
+//		for (int i = 0; i < weights.length; i++) {
+//
+//			for (int j = 0; j < weights[i].length; j++) {
+//
+//				//				System.out.print(i + "," + j + "   :");
+//				//				System.out.println(weights[i][j]);
+//				console << weights[i][j]
+//			}
+//		}
+//		console
 	}
 }

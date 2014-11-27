226fdeb2f25403798bcaa731e8f0b5733e86013b
mars <cosmo_mars@outlook.com>
2014-11-27 17:12:51 +0800

    test

--- know-heart/src/main/groovy/com/qubaopen/survey/controller/user/UserController.groovy
+++ know-heart/src/main/groovy/com/qubaopen/survey/controller/user/UserController.groovy
@@ -476,6 +476,7 @@ class UserController extends AbstractBaseController<User, Long> {
 			
 			request.setAttribute('map', result)
 			request.getRequestDispatcher("http://10.0.0.8/doctor/web/index.html")
+			return result
 		}
 
 		'{"success" : "0", "message": "亲，您输入的帐号或密码有误哟！"}'

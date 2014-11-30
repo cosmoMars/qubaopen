cc4bf564fb0ee944fce4e21f95cff56fafe60863
mars <cosmo_mars@outlook.com>
2014-11-30 12:15:16 +0800

    repair self

---
+++ qubaopen-core/src/main/java/com/qubaopen/survey/entity/doctor/DoctorMoney.java
@@ -0,0 +1,12 @@
+package com.qubaopen.survey.entity.doctor;
+
+import com.qubaopen.core.entity.AbstractBaseEntity;
+
+public class DoctorMoney extends AbstractBaseEntity<Long> {
+
+	private static final long serialVersionUID = 9111737815011960894L;
+
+	private Doctor doctor;
+	
+	
+}
